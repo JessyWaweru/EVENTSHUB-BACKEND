e9423e6ba249dbe131ed59620b5bcdc16bc89a34
Jessy <bryanjessywaweru@gmail.com>
2026-02-25 13:44:38 -0800

    :sparkles:added CORS

--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'django.contrib.admin',
+    'corsheaders',
     'events',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +124,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS Settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Default Vite port
+    "http://localhost:3000",  # Default Create React App port (just in case)
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:3000",
+]
+
+# Optional: If you plan to send credentials (like cookies or authorization headers) from React
+CORS_ALLOW_CREDENTIALS = True
