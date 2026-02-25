cbd4079ca9234a6b7f0e261fa3039ac56dddffbd
Jessy <bryanjessywaweru@gmail.com>
2026-02-25 13:39:47 -0800

    :sparkles:added views

--- events/views.py
+++ events/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Sponsor, Event, Speaker, Attendee
+from .serializers import (
+    UserSerializer, SponsorSerializer, EventSerializer, 
+    SpeakerSerializer, AttendeeSerializer
+)
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class SponsorViewSet(viewsets.ModelViewSet):
+    queryset = Sponsor.objects.all()
+    serializer_class = SponsorSerializer
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class SpeakerViewSet(viewsets.ModelViewSet):
+    queryset = Speaker.objects.all()
+    serializer_class = SpeakerSerializer
+
+class AttendeeViewSet(viewsets.ModelViewSet):
+    queryset = Attendee.objects.all()
+    serializer_class = AttendeeSerializer
