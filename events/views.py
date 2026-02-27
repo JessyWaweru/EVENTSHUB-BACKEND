f525a3c073db9dd941b54c533e4e7e6e4bba6d58
Jessy <bryanjessywaweru@gmail.com>
2026-02-27 02:04:14 -0800

    :bug:fixed auth issue with events in homepage

--- events/views.py
+++ events/views.py
@@ -1,31 +1,34 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import User, Sponsor, Event, Speaker, Attendee
 from .serializers import (
     UserSerializer, SponsorSerializer, EventSerializer, 
     SpeakerSerializer, AttendeeSerializer
 )
 
-# With Clerk authentication now set as the default, all authentication logic
-# (registration, login, token verification) is handled by the ClerkAuthentication class.
-# The views below are standard ModelViewSets for CRUD operations.
-# Access to these views is now protected by Clerk JWTs by default.
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    # This is the magic line! Guests can view (GET), but only logged-in users can create/edit (POST, PUT, DELETE)
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    # Explicitly lock down the other endpoints
+    permission_classes = [IsAuthenticated]
 
 class SponsorViewSet(viewsets.ModelViewSet):
     queryset = Sponsor.objects.all()
     serializer_class = SponsorSerializer
-
-class EventViewSet(viewsets.ModelViewSet):
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
 
 class SpeakerViewSet(viewsets.ModelViewSet):
     queryset = Speaker.objects.all()
     serializer_class = SpeakerSerializer
+    permission_classes = [IsAuthenticated]
 
 class AttendeeViewSet(viewsets.ModelViewSet):
     queryset = Attendee.objects.all()
     serializer_class = AttendeeSerializer
+    permission_classes = [IsAuthenticated]
